b972fb332963f811ff9fbaf8289f907176aa1f3a
pain4metoo <pain4metoo@gmail.com>
2024-05-21 12:05:28 +0300

    feat: add attribute for browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import 'normalize.css';
 import './scss/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
